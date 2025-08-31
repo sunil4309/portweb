1563abc9a2885a0c7fdc9fbf10b92eeccf24f21c
sunil4309 <sonupanda0075@gmail.com>
2025-08-31 23:07:53 +0530

    updated website

--- js/pcspec.js
+++ js/pcspec.js
@@ -1,13 +1,13 @@
 const pcProducts = [
   {
     name: "Graphic Card",
-    description: "RTX 4060, 8GB,",
+    description: "RTX 4060 | 8GB,",
     price: "₹ 92,000",
     img: "spec/rtx 4060.png"
   },
   {
     name: "Processor",
-    description: "Ryzen 7, 5700X Desktop Processor,cores 16 Threads 36 MB Cache 3.4 GHz Upto 4.6 GHz",
+    description: "Ryzen 7 | 5700X Desktop Processor | cores 16 Threads 36 MB Cache 3.4 GHz Upto 4.6 GHz",
     price: "₹ 38,000",
     img: "spec/5700x.png"
   },
@@ -24,10 +24,47 @@ const pcProducts = [
     img: "spec/pk650d.png"
   },
     {
-    name: "Corsair Vengeance 32GB RAM",
+    name: "Liquid cooler",
     description: "Deepcool LE520 240mm ARGB AIO / CPU Liquid Liquid Cooling Cabinet Cooler",
     price: "₹ 8,599",
     img: "spec/le520.png"
+  },
+                                                            // add live tech keyboard mouse
+    {
+    name: "Monitor (1)",
+    description: "Acer EK240Y E 23.8 Inch IPS Full HD | 1 MS VRB, 100Hz Refresh | 250 Nits | AMD Free Sync",
+    price: "₹ 9,499",
+    img: "spec/EK241.png"
+  },
+     {
+    name: "Monitor (2)",
+    description: "Samsung B2230 21.5 inch LCD Monitor | Response Time 5 ms | 60 Hz Refresh Rate",
+    price: "₹ 4,999",
+    img: "spec/B2230.png"
+  },
+    {
+    name: "Keyboard",
+    description: "60% Wired RGB Gaming Keyboard | Mechanical Keyboard with Linear Red Switch | Pro Driver Support",
+    price: "₹ 8,599",
+    img: "spec/K630i.png"
+  },
+    {
+    name: "Mouse",
+    description: "Ultra Value 8 Button Programmable | Gaming Grade DPI 200 to 12800 | Ultra-Responsive 7000fps ",
+    price: "₹ 1,299",
+    img: "spec/Blaze.png"
+  },
+    {
+    name: "Headphone",
+    description: "Wired Stereo Gaming | Detachable Noise Canceling Mic",
+    price: "₹ 3,990",
+    img: "spec/Eksa.png"
+  },
+     {
+    name: "Controller",
+    description: "1000Hz Polling Rate | Hallsense™ Magnetic Hall 3D Joysticks & Triggers | EZ Click Macros | Dual Vibration",
+    price: "₹ 2,499",
+    img: "spec/Elitex2.png"
   },
 ];
 
